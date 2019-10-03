49c33530a202117b08de59891cc846c0d579e9d9
Nick Butts <nbutts@appareo.com>
2019-10-03 14:06:13 -0700

    Checked in

--- CVHelp.py
+++ CVHelp.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 class CVHelp():
@@ -30,14 +30,14 @@ class CVHelp():
         if w > max_width:
             ratio = max_width / w
             new_height = int(h * ratio)
-            img = cv2.resize(img, (max_width, new_height))
+            img = cv.resize(img, (max_width, new_height))
 
         w = img.shape[1]
         h = img.shape[0]
         if h > max_height:
             ratio = max_height / h
             new_width = int(w * ratio)
-            img = cv2.resize(img, (new_width, max_height))
+            img = cv.resize(img, (new_width, max_height))
 
         return img
 
@@ -51,18 +51,18 @@ class CVHelp():
         delay -- how long to wait for a key
         """
         img = self.resize(img, self.max_width, self.max_height)
-        cv2.imshow(title, img)
-        cv2.waitKey(delay)
+        cv.imshow(title, img)
+        cv.waitKey(delay)
 
     def display_channels(self, img):
         """Helper function to display the separate color channels
         Keyword arguments:
         img -- the image to display
         """
-        cv2.imshow("Red", img[:,:,2])
-        cv2.imshow("Green", img[:,:,1])
-        cv2.imshow("Blue", img[:,:,0])
-        cv2.waitKey(1)
+        cv.imshow("Red", img[:,:,2])
+        cv.imshow("Green", img[:,:,1])
+        cv.imshow("Blue", img[:,:,0])
+        cv.waitKey(1)
 
     def generate_grid(self, imgs, grid_width, grid_height, labels = None):
         """Helper function to generate a grid of images. It is currently
@@ -85,13 +85,13 @@ class CVHelp():
             ri = self.resize(img, img_width, img_height)
             if len(ri.shape) == 2:
                 # Convert to color
-                ri = cv2.cvtColor(ri, cv2.COLOR_GRAY2BGR)
+                ri = cv.cvtColor(ri, cv.COLOR_GRAY2BGR)
             xstart = x * img_width
             ystart = y * img_height
             new_img[ystart:ystart + ri.shape[0], xstart:xstart + ri.shape[1], :] = ri
             if labels is not None:
                 #print("i: {:} -- x/y: {:}/{:} -- label: {:}".format(i, xstart, ystart, labels[i]))
-                cv2.putText(new_img, labels[i], (xstart + 10, ystart + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
+                cv.putText(new_img, labels[i], (xstart + 10, ystart + 20), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255))
 
             x += 1
             if x >= grid_width:
@@ -100,25 +100,25 @@ class CVHelp():
 
         return new_img
 
-    def generate_2dhist(self, image, channels=[0, 1], hist_scale = 10, Mask=None, histSize=[180, 256], ranges=[0,180,0,256]):   
+    def generate_2dhist(self, image, channels=[0, 1], hist_scale = 10, Mask=None, histSize=[180, 256], ranges=[0,180,0,256]):
         """Helper function to generate a 2D histogram that can be displayed
         Keyword arguments:
         image -- the image to plot
         channels -- which channels to plot
         Mask -- Mask to use for the histogram
         histSize -- The number of bins for each channel
-        ranges -- The min and max range for each channel        
-        """        
+        ranges -- The min and max range for each channel
+        """
         hsv_map = np.zeros((histSize[0], histSize[1], 3), np.uint8)
         h, s = np.indices(hsv_map.shape[:2])
         hsv_map[:,:,0] = h
         hsv_map[:,:,1] = s
         hsv_map[:,:,2] = 255
-        hsv_map = cv2.cvtColor(hsv_map, cv2.COLOR_HSV2BGR)
+        hsv_map = cv.cvtColor(hsv_map, cv.COLOR_HSV2BGR)
 
         #dark = hsv[...,2] < 32
-        #hsv[dark] = 0 
-        hist = cv2.calcHist(image, channels, None, histSize, ranges)
+        #hsv[dark] = 0
+        hist = cv.calcHist(image, channels, None, histSize, ranges)
         hist = np.clip(hist*0.005*hist_scale, 0, 1)
         vis = hsv_map*hist[:,:,np.newaxis]
         vis = vis.astype('uint8')
@@ -133,27 +133,36 @@ class CVHelp():
         image_to_mask -- the image to apply the back project mask to
         image_to_generate_mask -- the image to use as the HS back project mask
         """
-        hsvt = cv2.cvtColor(image_to_mask, cv2.COLOR_BGR2HSV)
-        roi  = cv2.cvtColor(image_to_generate_mask, cv2.COLOR_BGR2HSV)
+        hsvt = cv.cvtColor(image_to_mask, cv.COLOR_BGR2HSV)
+        roi  = cv.cvtColor(image_to_generate_mask, cv.COLOR_BGR2HSV)
 
         # Generate histograms
-        #hist_target = cv2.calcHist([hsvt], [0, 1], None, [180, 256], [0, 180, 0, 256])
-        hist_roi    = cv2.calcHist([roi],  [0, 1], None, [180, 256], [0, 180, 0, 256])
+        #hist_target = cv.calcHist([hsvt], [0, 1], None, [180, 256], [0, 180, 0, 256])
+        hist_roi    = cv.calcHist([roi],  [0, 1], None, [180, 256], [0, 180, 0, 256])
 
 
         # normalize histogram and apply backprojection
-        cv2.normalize(hist_roi,hist_roi,0,255,cv2.NORM_MINMAX)
-        dst = cv2.calcBackProject([hsvt],[0,1],hist_roi,[0,180,0,256],100)
-        cv2.imwrite('dst.bmp', dst)
+        cv.normalize(hist_roi,hist_roi,0,255,cv.NORM_MINMAX)
+        dst = cv.calcBackProject([hsvt],[0,1],hist_roi,[0,180,0,256],100)
+        cv.imwrite('dst.bmp', dst)
         # Now convolute with circular disc
-        disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-        cv2.filter2D(dst,-1,disc,dst)
+        disc = cv.getStructuringElement(cv.MORPH_ELLIPSE,(5,5))
+        cv.filter2D(dst,-1,disc,dst)
         # threshold and binary AND
-        ret,thresh = cv2.threshold(dst,50,255,0)
-        cv2.imwrite('thresh.bmp', thresh)
-        thresh = cv2.merge((thresh,thresh,thresh))
-        cv2.imwrite('merge.bmp', thresh)
-        res = cv2.bitwise_and(image_to_mask,thresh)
-        cv2.imwrite('res.bmp', res)
+        ret,thresh = cv.threshold(dst,50,255,0)
+        cv.imwrite('thresh.bmp', thresh)
+        thresh = cv.merge((thresh,thresh,thresh))
+        cv.imwrite('merge.bmp', thresh)
+        res = cv.bitwise_and(image_to_mask,thresh)
+        cv.imwrite('res.bmp', res)
         #res = np.vstack((image_to_mask, thresh, res))
         return image_to_mask, thresh, ret
+
+    def info(self, image):
+        """This prints info on the image:
+        width x height x channels - image type
+
+        Keyword arguments:
+        image -- the image to get info on
+        """
+        print("{} x {} x {} - {}".format(image.shape[1], image.shape[0], image.shape[2], image.dtype))
---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Nick Butts
+# @Date:   2019-10-02 13:26:25
+# @Last Modified by:   Nick Butts
+# @Last Modified time: 2019-10-02 13:27:55
+
+from .CVHelp import CVHelp
